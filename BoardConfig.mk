048fe3cf66bb112d1e6f3038a797b2b5456f3d62
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-03-01 20:01:47 -0500

    bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 include device/sony/yoshino/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := unknown
-ifneq (,$(filter %g8441,$(TARGET_PRODUCT)))
+ifneq (,$(filter %hentai_lilac_RoW,$(TARGET_PRODUCT)))
 TARGET_BOOTLOADER_BOARD_NAME := G8441
 else
 TARGET_BOOTLOADER_BOARD_NAME := G8441
@@ -26,6 +26,16 @@ endif
 PRODUCT_PLATFORM := yoshino
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=lilac
+
+#Use Clang instead of Gcc 4.9
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_CLANG_VERSION := r353983c
+
+#kernel
+BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_SOURCE := kernel/sony/msm-4.14/kernel
+TARGET_NEEDS_DTBOIMAGE := false
 
 # Partition information
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
